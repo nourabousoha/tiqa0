7caa1b705830df6f4f4aae5f20c4f8feedcdc9cc
Sghir Nouredine <nourabousoha@gmail.com>
2017-12-21 10:38:36 +0000

    migrate to router v4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import style from './style.css'
+import {Link,NavLink} from 'react-router-dom'
 /*just for a merge test */
 class Header extends React.Component {
     render() {
@@ -9,13 +10,13 @@ class Header extends React.Component {
             <nav className="navbar navbar-inverse top">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <a className="navbar-brand" href="#">TIQA shared <i className="fa fa-handshake-o shared" aria-hidden="true" ></i>confidence</a>
+                        <Link className="navbar-brand" to="/">TIQA shared <i className="fa fa-handshake-o shared" aria-hidden="true" ></i>confidence</Link>
                     </div>
                     <ul className="nav navbar-nav">
-                        <li className="active"><a href="/">Home</a></li>
+                        <li><NavLink exact={true} activeClassName="isactive" to="/">Home</NavLink></li>
                         
-                        <li><a href="dev">Developers</a></li>
-                        <li><a href="jobs">Jobs</a></li>
+                        <li><NavLink activeClassName="isactive" to="/dev">Developers</NavLink></li>
+                        <li><Link to="/jobs">Jobs</Link></li>
                     </ul>
                 </div>
             </nav>)
--- src/index.js
+++ src/index.js
@@ -1,38 +1,34 @@
 import 'babel-polyfill'
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Match, Miss } from 'react-router';
+import {BrowserRouter as Router, Route,Switch } from 'react-router-dom'
 import App from './components/App';
 import NotFound from './components/NotFound';
 import Front from './components/Front';
 import {loadDevs} from './states/actions'
 import { createStore ,applyMiddleware } from 'redux'
 import devApp from './states/reducers'
-//import { addDev } from './states/actions';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
-/*import {
-  ADD_DEV,
-  REMOVE_DEV,
-  LOAD_DEVS
-} from './states/actions'
-*/
+
 let store = createStore(devApp,{},applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
   store.dispatch(loadDevs());
 console.log(store.getState())
+
 const Root = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter basename="/taqa0/">
-        <div>
-          <Match exactly pattern="/" component={Front} />
-          <Match exactly pattern="/dev" component={App} />
-          <Miss component={NotFound} />
-        </div>
-      </BrowserRouter>
+      <Router >
+        
+        <Switch>
+          <Route exact path="/" component={Front} />
+          <Route path="/dev" component={App} />
+          <Route path="*" component={NotFound} />
+          </Switch>
+        
+      </Router>
     </Provider>
   )
 }
-
 render(<Root />, document.querySelector('#main'));
