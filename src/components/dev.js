6294d0b6d7575fff72e7c5681e6fca17d12ef04b
Sghir Nouredine <nourabousoha@gmail.com>
2017-12-22 14:15:48 +0000

    refactor dev separate Edit dev to EditDev.js

--- src/components/dev.js
+++ src/components/dev.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { removeDev } from '../states/actions'
 import { toggleEditDev } from '../states/actions'
+import EditDEv from './EditDev'
 /**
  * render the dev componenet 
  */
@@ -18,7 +19,7 @@ function DevEditRemove(props){
                     </a>
 
             </div>
-            <div className="input-group col-md-1 col-md-offset-2">
+            <div className="input-group col-md-1 col-md-offset-1">
                 <a className="form-control btn btn-success dev-edit-remove-button" type="submit" onClick={edit}
                     key={val.id}><i className="fa fa-pencil-square-o" aria-hidden="true">&nbsp;</i>
                     </a>
@@ -29,7 +30,6 @@ function DevEditRemove(props){
     </div>
     )
 }    
-
 function RenderDev(props){
     const val = props.val
     const edit = props.edit
@@ -100,47 +100,11 @@ class Dev extends React.Component {
     renderDevOrEditField() {
        if (this.props.toggleEdit ===this.val.id) {
             // handle  edit fields here
-            return <div className="container"> <div className="row col-md-12 text-center edit-form-container" >
-
-                <div className="row col-md-6 col-md-offset-3 ">
-                    <form ref="_devform" className="form form-horizontal" >
-                        <div className="form-group">
-                            <label htmlFor="devname" className="control-label col-sm-2">Name: </label>
-                            <div className="col-sm-6">   <input id="devname" onChange={this.onChange} className="form-control" ref="_name" type="text" placeholder="your name here..." value={this.val.name}required /></div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="devdesc" className="control-label col-sm-2">Skills:</label>
-                            <div className="col-sm-6"> <input id="devdesc" className="form-control" ref="_desc" type="text" placeholder="tell us more about you..." required value={this.val.desc}/></div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="devdesc" className="control-label col-sm-2">Photo:</label>
-                            <div className="col-sm-6">   <input id="devphoto" className="form-control" ref="_photo" type="text" placeholder="photo url..." required value={this.val.image}/></div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="devemail" className="control-label col-sm-2">Email address:</label>
-                            <div className="col-sm-6">  <input type="email" className="form-control" id="devemail" ref="_email" aria-describedby="emailHelp" placeholder="Enter email" ></input></div>
-                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="devpassword" className="control-label col-sm-2">Password</label>
-                            <div className="col-sm-6"> <input type="password" className="form-control" id="devpassword" placeholder="Password" ref="_password"></input></div>
-                        </div>
-                        <div className="form-check">
-                            <label className="control-label form-check-label col-sm-2" >I'am available</label>
-                           <input type="checkbox" className="form-check-input" ref="_status"></input>
-
-                        </div>
-                        <div className="row cancel-update">
-                        <button className="btn btn-danger" onClick={this.cancel}>Cancel</button>{this.updateDev} 
-                        <button className="btn btn-success" onClick={this.updateDev}>Update</button>
-                        </div>
-                    </form>
-                </div>
-            </div></div>
+           return <EditDEv val={this.val} cancel={this.cancel} updateDev={this.updateDev}/>
 
             // Handle rendering.
         } else {
-            return     <div class="container">
+            return     <div className="container">
                         <RenderDev val={this.val} edit={this.edit} remove={this.remove}/> 
                        
                        </div>
